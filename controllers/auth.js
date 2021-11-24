5bf385d083dff5540419ec8eee213e7cbb5384db
Marcos Cardoso <76229820+MagicMarcos@users.noreply.github.com>
2021-11-24 18:39:33 -0500

    fixed redirecting issues in auth

--- controllers/auth.js
+++ controllers/auth.js
@@ -39,7 +39,7 @@ exports.postLogin = (req, res, next) => {
         return next(err);
       }
       req.flash("success", { msg: "Success! You are logged in." });
-      res.redirect(req.session.returnTo || "/profile");
+      res.redirect(req.session.returnTo || "/feed");
     });
   })(req, res, next);
 };
@@ -56,7 +56,7 @@ exports.logout = (req, res) => {
 
 exports.getSignup = (req, res) => {
   if (req.user) {
-    return res.redirect("/profile");
+    return res.redirect("/feed");
   }
   res.render("signup", {
     title: "Create Account",
@@ -108,7 +108,7 @@ exports.postSignup = (req, res, next) => {
           if (err) {
             return next(err);
           }
-          res.redirect("/profile");
+          res.redirect("/feed");
         });
       });
     }
